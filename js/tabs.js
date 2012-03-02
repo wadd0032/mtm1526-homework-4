10d36c3e50e8fff9f540685b3fd4fcbc6aaa8368
wadd0032 <deannawadden@rogers.com>
2012-03-02 17:55:12 -0500

    finalized coding for show/hide function for content areas

--- js/tabs.js
+++ js/tabs.js
@@ -1,1 +1,10 @@
 // JavaScript Document
+
+$(document).ready(function() {
+	$('.tabs a').on('click', function (ev) {
+		var tabToShow = $(this).attr('href');
+		
+		$('.content div').hide('slow');
+		$(tabToShow).show('slow');
+	});
+});
